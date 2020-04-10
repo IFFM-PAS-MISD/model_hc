921d34c2dafdec4df8ef994e5bb043b81c992082
pfiborek <pfiborek@imp.gda.pl>
2020-04-10 15:41:57 +0200

    revised version

--- src/tools/matplotlib/madif.py
+++ src/tools/matplotlib/madif.py
@@ -24,6 +24,7 @@ f = data_fft[:,0]
 frequency = (f==16.5e3)
 maxValueFFT = np.amax(data_fft[frequency,1])
 
+
 x_d = np.arange(0,140,20)
 xp = np.linspace(x_d[0],x_d[-1],100)
 fig = plt.figure(figsize=cm2inch(18.4,8))
@@ -39,6 +40,6 @@ l2 = ax.plot(xp,yp(xp),'b')[0]
 plt.xlabel('d [mm]')
 plt.ylabel('I [-]')
 
-plt.subplots_adjust(left=0.10, bottom=0.15, right=0.90, top=1.00, wspace=0.23, hspace=0.30)
+plt.subplots_adjust(left=0.10, bottom=0.15, right=0.90, top=0.95, wspace=0.23, hspace=0.30)
 plt.savefig("E:/model_hc/reports/figures/eps/madif.eps", format="eps")
 plt.show()
